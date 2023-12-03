00074c9e5d51c15d3eabee385f268ba429707ed4
urosspasenovic <57662604+urosspasenovic@users.noreply.github.com>
2023-12-03 10:42:18 +0100

    State pattern WIP

---
+++ Assets/Scripts/Movement/FallingDownState.cs
@@ -0,0 +1,25 @@
+namespace Movement
+{
+    public class FallingDownState : State
+    {
+        public void StraightLeft()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void StraightRight()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void UpLeft()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void UpRight()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Assets/Scripts/Movement/State.cs
@@ -0,0 +1,7 @@
+public interface State
+{
+    public void StraightLeft();
+    public void StraightRight();
+    public void UpLeft();
+    public void UpRight();
+}
