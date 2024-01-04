9d671f95fcf09eb20a2359556a44907608d57426
uros <uross@twodesperados.com>
2024-01-04 12:56:58 +0100

    Introduce first combo

--- Assets/Scripts/Movement/StandingState.cs
+++ Assets/Scripts/Movement/StandingState.cs
@@ -4,6 +4,9 @@ namespace Movement
 {
     public class StandingState : State
     {
+        private int _framesTillPossibleCombo = 20;
+        private int _currentMovementFrameCount;
+
         public StandingState(PlayerMovement playerMovement, Transform playerTransform, Rigidbody2D rigidbody2D, AnimationController animationController)
             : base(playerMovement, playerTransform, rigidbody2D, animationController)
         {
@@ -11,15 +14,25 @@ namespace Movement
         
         public override void StraightMovement(float deltaXMovement, float movementSpeed, bool direction)
         {
+            _currentMovementFrameCount++;
             Rigidbody2D.velocity = new Vector2(0, Rigidbody2D.velocity.y);
-            PlayerTransform.position += Vector3.right  * (deltaXMovement * movementSpeed *  Time.deltaTime);
+            PlayerTransform.position += Vector3.right * (deltaXMovement * movementSpeed * Time.deltaTime);
             AnimationController.RotatePlayer(direction);
         }
 
         public override void UpAndHorizontalMovement(Vector2 jumpAngle, float movementSpeed, bool direction)
         {
-            PlayerMovement.ChangeState(States.UpMovementState);
-            Rigidbody2D.AddForce(jumpAngle.normalized * movementSpeed);
+            if (_currentMovementFrameCount >= _framesTillPossibleCombo)
+            {
+                FirstComboJump(movementSpeed, jumpAngle.x);
+                //todo change to combo state
+                PlayerMovement.ChangeState(States.UpMovementState);
+            }
+            else
+            {
+                PlayerMovement.ChangeState(States.UpMovementState);
+                Rigidbody2D.AddForce(jumpAngle.normalized * movementSpeed);
+            }
             AnimationController.RotatePlayer(direction);
         }
 
@@ -36,7 +49,17 @@ namespace Movement
         
         public override void ExitState()
         {
-            
+            _framesTillPossibleCombo = 5;
+            _currentMovementFrameCount = 0;
+        }
+
+        private void FirstComboJump(float movementSpeed, float direction)
+        {
+            var jumpAngle = new Vector2(1 * direction,2).normalized;
+            var comboCounter = 1;
+            Rigidbody2D.AddForce(jumpAngle * movementSpeed * comboCounter);
+            _currentMovementFrameCount = 0;
+            Debug.LogError("Combooooooo");
         }
     }
 }
